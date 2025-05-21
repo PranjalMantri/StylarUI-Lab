93677c83a1dd3b2fc68932aa6ac5ef85e3329382
Pranjal Mantri <pranjalmantri@gmail.com>
2025-05-21 17:22:34 +0530

    feat: add react-router-dom and implement routing for LandingPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+
 function App() {
-  return <div className="text-4xl">Hello world</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+  );
 }
 
 export default App;
