ed2351c4d83f155d6d4e7daeab6efba3f60dbd47
Pranjal Mantri <pranjalmantri@gmail.com>
2025-05-23 00:06:01 +0530

    fix: fix component configuration width in mobile devices

--- src/components/playground/ComponentConfiguration.tsx
+++ src/components/playground/ComponentConfiguration.tsx
@@ -15,8 +15,8 @@ const ComponentConfiguration: React.FC<ComponentConfigurationProps> = ({
   currentComponent,
 }) => {
   return (
-    <div className="p-6 w-1/3 flex-shrink-0">
-      <section className="bg-slate-800/50 border border-slate-700/70 rounded-xl p-4 sm:p-6 shadow-2xl overflow-y-auto max-h-[calc(100vh-80px)">
+    <div className="p-6 w-screen md:w-1/3 flex-shrink-0">
+      <section className="bg-slate-800/50 border border-slate-700/70 rounded-xl p-4 sm:p-6 shadow-2xl max-h-[calc(100vh-80px)">
         <div className="flex items-center gap-3 mb-6 pb-4 border-b border-slate-700">
           <SlidersHorizontal className="w-5 h-5 text-sky-400" />
           <h2 className="text-xl font-semibold text-slate-100">
