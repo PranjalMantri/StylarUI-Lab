37f88416df06b0fb2b29701d178ecb6a39333d28
Pranjal Mantri <pranjalmantri@gmail.com>
2025-05-25 22:07:43 +0530

    feat: implement dynamic rendering of ButtonCodeExporter based on selected component

--- src/components/playground/CodeExporter.tsx
+++ src/components/playground/CodeExporter.tsx
@@ -1,9 +1,28 @@
+import { Code } from "lucide-react";
 import ButtonCodeExporter from "../Button/CodeExporter/ButtonCodeExporter";
+import useComponentStore from "../../store/componentStore";
 
 function CodeExporter() {
+  const { currentComponent } = useComponentStore();
+
+  const renderExporter = () => {
+    switch (currentComponent.id) {
+      case "button":
+        return <ButtonCodeExporter />;
+      default:
+        return <div>Select a component to get code</div>;
+    }
+  };
+
   return (
-    <div>
-      <ButtonCodeExporter />
+    <div className="bg-slate-800/50 border border-slate-700/70 w-full m-3 rounded-xl p-4 sm:p-6 shadow-2xl flex-grow flex flex-col">
+      <div className="flex items-center gap-3 mb-4 sm:mb-2">
+        <Code className="w-6 h-6 text-sky-400" />
+        <h2 className="text-xl font-semibold text-slate-100">
+          Export Component
+        </h2>
+      </div>
+      {renderExporter()}
     </div>
   );
 }
