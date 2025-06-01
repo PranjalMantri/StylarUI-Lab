8a5b90c24d56cfaad6ba29e697fa9a15ee2458d9
Pranjal Mantri <pranjalmantri@gmail.com>
2025-06-01 21:51:05 +0530

    feat: implement label options in Dropdown Preview

--- src/components/Dropdown/Dropdown-Preview/DropdownPreview.tsx
+++ src/components/Dropdown/Dropdown-Preview/DropdownPreview.tsx
@@ -1,5 +1,37 @@
+import React from "react";
+import useDropdownStore from "../../../store/dropdownStore";
+import { fontSizeMap, fontWeightMap } from "./styleMaps";
+
 function DropdownPreview() {
-  return <div>DropdownPreview</div>;
+  const { label, labelColor, labelFontSize, labelFontWeight } =
+    useDropdownStore();
+
+  const options = [
+    { value: "option1", label: "Option 1" },
+    { value: "option2", label: "Option 2" },
+    { value: "option3", label: "Option 3" },
+  ];
+
+  const labelStyles: React.CSSProperties = {
+    color: labelColor,
+    fontSize: fontSizeMap[labelFontSize],
+    fontWeight: fontWeightMap[labelFontWeight],
+  };
+
+  return (
+    <div className="flex flex-col gap-2 items-start">
+      <label htmlFor="dropdown" style={labelStyles}>
+        {label}
+      </label>
+      <select id="dropdown" name="dropdown">
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
 
 export default DropdownPreview;
