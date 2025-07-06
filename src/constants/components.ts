a9a9833798afdd9299309d5feabbdce32cc799a6
Pranjal Mantri <pranjalmantri@gmail.com>
2025-07-06 16:39:36 +0530

    refactor: remove disabled components from COMPONENTS array for cleaner configuration

--- src/constants/components.ts
+++ src/constants/components.ts
@@ -12,29 +12,10 @@ export const COMPONENTS: Array<{
     iconName: "ChevronsUpDown",
   },
   { id: "checkbox", name: "Checkbox", iconName: "CheckSquare" },
-  { id: "radio", name: "Radio Group", iconName: "List", disabled: true },
-  {
-    id: "toggle",
-    name: "Toggle Switch",
-    iconName: "ToggleLeft",
-    disabled: true,
-  },
-  {
-    id: "alert",
-    name: "Alert Message",
-    iconName: "AlertTriangle",
-    disabled: true,
-  },
   { id: "slider", name: "Slider", iconName: "Sliders" },
   {
     id: "spinner",
     name: "Loading Spinner",
     iconName: "Loader2",
-  },
-  {
-    id: "datepicker",
-    name: "Date Picker",
-    iconName: "CalendarDays",
-    disabled: true,
   },
 ];
